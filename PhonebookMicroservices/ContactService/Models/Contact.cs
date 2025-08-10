0cc418b075c5ce9a8787611a0b7c5099ec480a63
AliihsanMuhziroglu <aliihsanmuhziroglu@gmail.com>
2025-08-10 12:19:58 +0300

    contact models added

---
+++ PhonebookMicroservices/ContactService/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContactService.Models
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Company { get; set; }
+        public List<ContactInfo> ContactInfos { get; set; } = new();
+    }
+}
---
+++ PhonebookMicroservices/ContactService/Models/ContactInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ContactService.Models
+{
+    public enum ContactInfoType
+    {
+        Phone = 0,
+        Email = 1,
+        Location = 2
+    }
+
+    public class ContactInfo
+    {
+        public Guid Id { get; set; }
+        public ContactInfoType Type { get; set; }
+        public string Value { get; set; } = null!;
+        public Guid ContactId { get; set; }
+        public Contact? Contact { get; set; }
+    }
+}
